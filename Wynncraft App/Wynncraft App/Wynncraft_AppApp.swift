753bd6e2b871162039a1036eecea4532e1b3e4eb
jaded-x <bayne.jaded@gmail.com>
2021-07-19 02:45:35 +1000

    Created swift project

---
+++ Wynncraft App/Wynncraft App/Wynncraft_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wynncraft_AppApp.swift
+//  Wynncraft App
+//
+//  Created by JJ B on 19/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Wynncraft_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
